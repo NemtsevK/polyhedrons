62a124f86865924cb77c5db225eaa6aa78b0f08e
Konstantin <mutecross@gmail.com>
2023-11-18 11:53:48 +0300

    add new pages and change styles

--- source/js/main.js
+++ source/js/main.js
@@ -3,13 +3,12 @@ const backgroundCanvas = solidsList.querySelectorAll('.Background');
 backgroundCanvas.forEach((element)=>{
     element.value = '#eeeeee';
 })
-// const canvasList = solidsList.querySelectorAll('canvas');
-// const solidsItem = solidsList.querySelector('.solids__item');
-// window.addEventListener('resize', () => {
-//     const width = solidsItem.scrollWidth;
-//     console.log(width);
-//     canvasList.forEach((element)=>{
-//         element.setAttribute('width', width.toString());
-//         element.setAttribute('height', width.toString());
-//     })
-// });
+const canvasList = solidsList.querySelectorAll('canvas');
+const solidsItem = solidsList.querySelector('.solids-item__content');
+
+canvasList.forEach((canvas)=>{
+//* window.devicePixelRatio // для ретина, но плохо крутится
+  canvas.width = solidsItem.clientWidth;
+  canvas.height = solidsItem.clientWidth;
+})
+
