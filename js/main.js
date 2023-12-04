b4dacd7e002d5f9ffe086b06979cd03cd5cd2eb4
Konstantin <mutecross@gmail.com>
2023-12-04 22:22:14 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-const solidsList = document.querySelector('.solids__list');
-const backgroundCanvas = solidsList.querySelectorAll('.Background');
-backgroundCanvas.forEach((element)=>{
-    element.value = '#ffffff';
-})
-const canvasList = solidsList.querySelectorAll('canvas');
-const solidsItem = solidsList.querySelector('.solids-item__content');
+const DEFAULT_BACKGROUND = '#f6f6f6';
 
-canvasList.forEach((canvas)=>{
+const solidsList = document.querySelector('.solids__list');
+const backgroundCanvas = solidsList.querySelectorAll('.solid__background');
+const canvasList = solidsList.querySelectorAll('.solid__canvas');
+const solidsItem = solidsList.querySelector('.solid__content');
+
+backgroundCanvas.forEach((element) => {
+  element.value = DEFAULT_BACKGROUND;
+})
+
+canvasList.forEach((canvas) => {
 //* window.devicePixelRatio // для ретина, но плохо крутится
   canvas.width = solidsItem.clientWidth;
   canvas.height = solidsItem.clientWidth;
